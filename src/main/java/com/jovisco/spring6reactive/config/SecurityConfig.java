40b49ab986b622af2ef8078e277e54b6945a722a
Josef Heiss <josef.heiss@jovisco.de>
2024-05-31 18:56:01 +0200

    after section 43 -actuator

--- src/main/java/com/jovisco/spring6reactive/config/SecurityConfig.java
+++ src/main/java/com/jovisco/spring6reactive/config/SecurityConfig.java
@@ -5,10 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
